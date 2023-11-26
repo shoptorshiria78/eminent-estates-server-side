7647e48723639d412bf033b5a3b5c73365784b12
shoptorshi <shoptorshiria78@gmail.com>
2023-11-26 10:37:58 +0600

    verify admin and verify member api has been made

--- index.js
+++ index.js
@@ -32,31 +32,53 @@ async function run() {
     const apartmentCollections = client.db("EminentEstates").collection("apartment");
     const agreementCollections = client.db("EminentEstates").collection("agreement");
     const userInfoCollections = client.db("EminentEstates").collection("users");
-    
+
     // middleware
-    const verifyToken = (req, res, next)=>{
-      if(!req.headers.authorization){
-        return res.status(401).send({message:"unauthorized access"})
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" })
       }
       const token = req.headers.authorization.split(' ')[1];
-      jwt.verify(token, process.env.Access_Token, (err, decoded)=>{
-        if(err){
-          return res.status(401).send({message:"unauthorized access"})
+      jwt.verify(token, process.env.Access_Token, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" })
         }
         req.decoded = decoded;
         next();
       })
-     
+
     }
-
-
-    // jwt token creation
-
-    app.post('/jwt', async(req, res)=>{
-      const user = req.body;
-      const token = jwt.sign(user, process.env.Access_Token,{expiresIn:'1h'})
-      res.send({token})
+    // verify admin
+    app.get('/user/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await userInfoCollections.findOne(query);
+      let admin = false;
+      if (user) {
+          admin = user?.role === 'admin';
+      }
+      res.send({admin})
     })
+    // verify member
+    app.get('/user/member/:email', async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await userInfoCollections.findOne(query);
+      let member = false;
+      if (user) {
+        member = user?.role === 'member';
+      }
+      res.send({member})
+    })
+
+
+
     // get the all apartments info
     app.get('/apartments', async (req, res) => {
       try {
@@ -68,23 +90,31 @@ async function run() {
       }
     })
 
+    // jwt token creation
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.Access_Token, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
     // post the user data
 
-    app.post ('/users', async(req, res)=>{
-      try{
+    app.post('/users', async (req, res) => {
+      try {
         const userInfo = req.body;
-        const query = {email: userInfo.email};
+        const query = { email: userInfo.email };
         const existingUser = await userInfoCollections.findOne(query);
-        if(existingUser){
-          return res.send({message:'user already exists', insertedId:null})
+        if (existingUser) {
+          return res.send({ message: 'user already exists', insertedId: null })
         }
-        const result =await userInfoCollections.insertOne(userInfo);
+        const result = await userInfoCollections.insertOne(userInfo);
         res.send(result);
       }
-      catch(error){
+      catch (error) {
         console.log(error)
       }
-      
+
     })
     // post the agreement request
     app.post('/agreement', async (req, res) => {
